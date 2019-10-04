3d99d1732c3a3c3af4b971ba21e56445e5d023c9
András Ács <acsandras@gmail.com>
2019-10-04 13:06:02 +0200

    Massere af forbedringer

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 /**
  * Developed by András Ács (acsandras@gmail.com)
  * Zealand / www.zealand.dk
@@ -12,39 +10,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Sten saks papir 1.0");
 
-        Haand kelvin = Haand.SAKS;
-        Haand henrik = Haand.PAPIR;
-        Haand mik = Haand.STEN;
-        Haand andras = Haand.STEN;
-
         // Her starter spillet
-        SteenSaksPapir runde1 = new SteenSaksPapir();
-        // System.out.println(runde1.slaa(kelvin, mik));
+        SteenSaksPapir.startSpillet();
 
-        // Computer generere en hånd
-        Haand computerensHaand = null;
-        int haandInt =(int) (Math.random()*3+1);
-        // Korte nørklet kode  computersHaand = Haand.values()[haandInt];
-        if (haandInt == 1) { computerensHaand = Haand.STEN; }
-        if (haandInt == 2) { computerensHaand = Haand.SAKS; }
-        if (haandInt == 3) { computerensHaand = Haand.PAPIR; }
-        System.out.println("Computer slår " + computerensHaand);
 
-        // Indlæse spillerns hånd
-        Scanner scanner = new Scanner(System.in);
-        Haand spillerensHaand = null;
-        String spillerenString = scanner.next();
-        if (spillerenString.equalsIgnoreCase("sten")) { spillerensHaand = Haand.STEN; }
-        if (spillerenString.equalsIgnoreCase("saks")) { spillerensHaand = Haand.SAKS; }
-        if (spillerenString.equalsIgnoreCase("papir")) { spillerensHaand = Haand.PAPIR; }
-        System.out.println("Spiller slår " + spillerensHaand);
-
-        // Sammenligne med slaa() metoden
-        int resultat = runde1.slaa(computerensHaand, spillerensHaand);
-
-        // Udråbe en vinder
-        System.out.println("Resultatet er " + resultat);
-        // TODO vis resultatet mere meningsfuldt
     }
 
 }
--- src/SteenSaksPapir.java
+++ src/SteenSaksPapir.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Developed by András Ács (acsandras@gmail.com)
  * Zealand / www.zealand.dk
@@ -6,6 +8,45 @@
  */
 
 public class SteenSaksPapir {
+
+    public static void startSpillet() {
+        SteenSaksPapir runde1 = new SteenSaksPapir();
+
+        int count = 1;
+        while (count>=1) {
+        // Indlæse spillerns hånd
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Runde " + count + " - hvad slår du (sten, saks, papir)? ");
+        Haand spillerensHaand = null;
+        String spillerenString = scanner.next();
+        if (spillerenString.equalsIgnoreCase("sten")) { spillerensHaand = Haand.STEN; }
+        if (spillerenString.equalsIgnoreCase("saks")) { spillerensHaand = Haand.SAKS; }
+        if (spillerenString.equalsIgnoreCase("papir")) { spillerensHaand = Haand.PAPIR; }
+        if (spillerenString.equalsIgnoreCase("afslut")) { break; }
+
+            System.out.println("Spiller slår " + spillerensHaand);
+
+        // Computer generere en hånd
+        Haand computerensHaand = null;
+        int haandInt =(int) (Math.random()*3+1);
+        // Korte nørklet kode  computersHaand = Haand.values()[haandInt];
+        if (haandInt == 1) { computerensHaand = Haand.STEN; }
+        if (haandInt == 2) { computerensHaand = Haand.SAKS; }
+        if (haandInt == 3) { computerensHaand = Haand.PAPIR; }
+        System.out.println("Computer slår " + computerensHaand);
+
+        // Sammenligne med slaa() metoden
+        int resultat = runde1.slaa(spillerensHaand, computerensHaand);
+
+        // Udråbe en vinder
+        if (resultat==1) { System.out.println("Du har vundet!"); }
+        if (resultat==2) { System.out.println("Du har tabt!"); }
+        if (resultat==0) { System.out.println("Uafgjort :)"); }
+        if (resultat==-1) { System.out.println("Der er sket en funky fejl, prøv igen!"); }
+
+        count++;
+        }
+    }
 
     /** Hvis den første hånd vinder, returneres 1
      *  Hvis den anden hånd vinder, returneres 2
